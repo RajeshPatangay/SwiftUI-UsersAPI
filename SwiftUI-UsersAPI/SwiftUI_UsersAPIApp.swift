9eb0151dd56a3844756cfff83f0ad92822db3fb6
Rajesh Patangay <rajesh.patangay@aa.com>
2022-09-04 16:38:00 -0500

    Initial Commit

---
+++ SwiftUI-UsersAPI/SwiftUI_UsersAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UsersAPIApp.swift
+//  SwiftUI-UsersAPI
+//
+//  Created by Rajesh Patangay on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UsersAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
